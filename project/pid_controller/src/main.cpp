d6abbc661fe95ba163c26a318c03b4bbafd3f47d
Vy Nguyen <datduong195@gmail.com>
2022-07-21 15:39:46 +0200

    Added: finish all implementations and 3 scenarios of PID

--- project/pid_controller/src/main.cpp
+++ project/pid_controller/src/main.cpp
@@ -226,10 +226,16 @@ int main ()
   **/
 
   PID pid_steer = PID();
-  pid_steer.Init(0.5, 0.05, 0.5, 1.0, -1.0);
+  // pid_steer.Init(0.5, 0.01, 0.5, 1.2, -1.2); //case 1
+  // pid_steer.Init(0.4, 0.001, 0.35, 1.2, -1.2); // case 2
+  pid_steer.Init(0.4, 0.001, 0.35, 1.2, -1.2); // case 3
+
+
 
   PID pid_throttle = PID();
-  pid_throttle.Init(0.5, 0.05, 0.5, 1.0, -1.0);
+  // pid_throttle.Init(0.5, 0.001, 0.5, 1.0, -1.0); //case 1
+  // pid_throttle.Init(0.2, 0.001, 0.02, 1.0, -1.0);// case 2
+pid_throttle.Init(0.2, 0.0015, 0.04, 1.0, -1.0); // case 3
 
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer, &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
   {
@@ -292,7 +298,7 @@ int main ()
           * TODO (step 3): uncomment these lines
           **/
 //           // Update the delta time with the previous command
-//           pid_steer.UpdateDeltaTime(new_delta_time);
+          pid_steer.UpdateDeltaTime(new_delta_time);
 
           // Compute steer error
           double error_steer;
@@ -303,23 +309,23 @@ int main ()
           /**
           * TODO (step 3): compute the steer error (error_steer) from the position and the desired trajectory
           **/
-//           error_steer = 0;
+          error_steer = angle_between_points(x_points[x_points.size() -2], y_points[y_points.size() -2], x_points[x_points.size() -1], y_points[y_points.size() -1]) - yaw;
 
           /**
           * TODO (step 3): uncomment these lines
           **/
 //           // Compute control to apply
-//           pid_steer.UpdateError(error_steer);
-//           steer_output = pid_steer.TotalError();
+          pid_steer.UpdateError(error_steer);
+          steer_output = pid_steer.TotalError();
 
 //           // Save data
-//           file_steer.seekg(std::ios::beg);
-//           for(int j=0; j < i - 1; ++j) {
-//               file_steer.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-//           }
-//           file_steer  << i ;
-//           file_steer  << " " << error_steer;
-//           file_steer  << " " << steer_output << endl;
+          file_steer.seekg(std::ios::beg);
+          for(int j=0; j < i - 1; ++j) {
+              file_steer.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+          }
+          file_steer  << i ;
+          file_steer  << " " << error_steer;
+          file_steer  << " " << steer_output << endl;
 
           ////////////////////////////////////////
           // Throttle control
